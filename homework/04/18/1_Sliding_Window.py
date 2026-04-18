0c20985d60c6805111532d00f2389602652905c4
Shipovmax <shipov.max@icloud.com>
2026-04-18 11:16:14 +0300

    add new task 1_Sliding_Window.py and 2_Two_Pointers.py and 3_Prefix_Sum_+_Hash_Map.py

---
+++ homework/04/18/1_Sliding_Window.py
@@ -0,0 +1,16 @@
+'''
+Longest Substring Without Repeating Characters
+Дана строка s. Найди длину наидлиннейшей подстроки
+без повторяющихся символов.
+Input:  s = "abcabcbb"
+Output: 3  # "abc"
+
+Input:  s = "pwwkew"
+Output: 3  # "wke"
+
+Input:  s = ""
+Output: 0
+Условия: решение за O(n), без встроенных функций
+типа collections.Counter для основной логики.
+'''
+
