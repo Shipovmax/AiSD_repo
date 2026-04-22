a3eebc659d0155fa758b03117a03ffc15b42c50b
Shipovmax <shipov.max@icloud.com>
2026-04-22 22:20:18 +0300

    pattern 3 complete

---
+++ homework/04/Interview/22/3_HashMap/10.py
@@ -0,0 +1,21 @@
+'''
+Найди индекс первого неповторяющегося символа в строке. 
+Если нет — верни -1.
+'''
+
+# Counter - Считает сколько есть каждого символа
+from collections import Counter  
+
+def first_uniq_char(s: str) -> int:
+    count = Counter(s)
+
+    for i, c in enumerate(s):
+        if count[c] == 1:
+            return i
+    
+    return -1
+
+# Тест
+print(first_uniq_char("leetcode"))   # 0
+print(first_uniq_char("loveleet"))   # 2
+print(first_uniq_char("aabb"))       # -1
