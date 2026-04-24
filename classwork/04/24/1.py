ca65007c1c14605d0c4b29f7498af40ca74d7412
Shipovmax <shipov.max@icloud.com>
2026-04-24 09:00:02 +0300

    complete task

---
+++ classwork/04/24/1.py
@@ -0,0 +1,29 @@
+a = 4
+
+# Большое решение
+
+def func(x: int) -> int :
+    return x**3
+rez = func(a)
+print(rez)
+
+# Короткое решение
+
+rez = lambda x:x**3
+print(rez)
+
+# ---------------------------------
+
+b = [4,2,3,6,7]
+
+rez = list(map(lambda x:x**3,b))
+print(rez)
+
+# ---------------------------------
+
+a =['aw', 'dfg', 'acpo']
+
+rez = list(filter(lambda x:x.upper() if x[0]=='a' else 0 , a))
+print(rez)
+
+rez = list(map(x**2, filter(lambda x: x / 2 == 0, a)))
