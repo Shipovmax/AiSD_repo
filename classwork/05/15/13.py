d99876b2ab186154d242f43610c3e68bfb7bc651
Shipov <shipov.max@icloud.com>
2026-05-15 09:54:06 +0300

    add StackList implementation with bracket checking functionality

---
+++ classwork/05/15/13.py
@@ -0,0 +1,48 @@
+from typing import Optional
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next: Optional["Node"] = None
+
+
+class StackList:
+    def __init__(self):
+        self.head: Optional[Node] = None
+
+    def push(self, value):
+        new_node = Node(value)
+        new_node.next = self.head
+        self.head = new_node
+
+    def pop(self):
+        if self.head is None:
+            return None
+
+        current_node = self.head
+        value = current_node.value
+        self.head = current_node.next
+        return value
+
+    def is_empty(self):
+        return self.head is None
+
+
+def check_brackets(code_string):
+    stack = StackList()
+    for char in code_string:
+        if char == "(":
+            stack.push(char)
+        elif char == ")":
+            if stack.is_empty():
+                return False
+            stack.pop()
+    return stack.is_empty()
+
+
+test_1 = "((dsasdas((das(d)das)d)asd)da)"
+test_2 = "(f(gfhd((hgfdg((fghh)dads)das))ds)ad)d(ads())))"
+
+print(check_brackets(test_1))
+print(check_brackets(test_2))
